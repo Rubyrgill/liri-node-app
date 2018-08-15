f7b42e85029f7885903deb76d1cd7c8f57eb08e4
RubyG <rubyrgill@hotmail.com>
2018-08-15 17:57:11 -0400

    function added, movie & dwis, fixed spotify issue

--- liri.js
+++ liri.js
@@ -35,7 +35,7 @@ switch (liriReturn) {
         spotifyThisSong();
         break;
 
-    case "move-this":
+    case "movie-this":
         movieThis();
         break;
 
@@ -44,10 +44,11 @@ switch (liriReturn) {
         break;
 }
 
+
 //TWITTER FUNCTION
 function myTweets() {
     //username
-    var parameter = { screen_name: "rubyrgill1" };
+    var parameter = { screen_name: "rubyrgill1", count: 20 };
     //grabbing information from user
     client.get("statuses/user_timeline", parameter, function (err, tweets, response) {
         //if error, then display
@@ -56,7 +57,7 @@ function myTweets() {
             return;
         } else {
             //otherwise, display recent 20 tweets
-            for (var i = 0; i < 21; i++) {
+            for (var i = 0; i < tweets.length; i++) {
                 console.log(tweets[i].text);
                 console.log(response);
             };
@@ -64,10 +65,12 @@ function myTweets() {
     });
 }
 
+var trackName = process.argv[3];
+
 //SPOTIFY FUNCTION
-function spotifyThisSong(trackName) {
+function spotifyThisSong() {
     //store user input 
-    var trackName = process.argv[3];
+
     //user input == null, take default selection
     if (!trackName) {
         trackName: "The Sign";
@@ -77,7 +80,7 @@ function spotifyThisSong(trackName) {
     songRequest = trackName;
     spotify.search({
         type: "track",
-        query: songRequest
+        query: songRequest,
     },
         function (err, data) {
             //if error, display
@@ -91,11 +94,11 @@ function spotifyThisSong(trackName) {
                     if (trackInfo[i] != undefined) {
                         var spotifyResults =
                             "Artist: " + trackInfo[i].artists[0].name + "\n" +
-                            "  Song: " + trackInfo[i].name + "\n" +
-                            "  Preview Link: " + trackInfo[i].preview_url + "\n" +
-                            " Album: " + trackInfo[i].album.name;
+                            "Song: " + trackInfo[i].name + "\n" +
+                            "Preview Link: " + trackInfo[i].preview_url + "\n" +
+                            "Album: " + trackInfo[i].album.name;
 
-
+                        console.log("___________________________")
                         console.log(spotifyResults)
                         console.log("___________________________")
                     };
@@ -106,8 +109,97 @@ function spotifyThisSong(trackName) {
 }
 
 //MOVIE FUNCTION
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+function movieThis() {
 
-request(queryUrl, function (error, response, body) {
+    //if no user input, display default
+    if (!movieName) {
+        movieName = "mr nobody";
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: ")
+        console.log("It's on Netflix!")
+    }
 
-})
+    //url 
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+
+
+    request(queryUrl, function (err, response, body) {
+        //if no error
+        if (!err && response.statusCode === 200) {
+            var movieData = JSON.parse(body);
+
+            //grabbing data from object
+            var queryUrlResults =
+                "Title: " + movieData.Title + "\n" +
+                "Year: " + movieData.Year + "\n" +
+                "IMDB Rating: " + movieData.Ratings[0].Value + "\n" +
+                "Rotten Tomatoes Rating: " + movieData.Ratings[1].Value + "\n" +
+                "Country: " + movieData.Country + "\n" +
+                "Language: " + movieData.Language + "\n" +
+                "Plot: " + movieData.Plot + "\n" +
+                "Actors: " + movieData.Actors + "\n";
+
+            console.log("______________________")
+            console.log(queryUrlResults);
+            console.log("______________________")
+
+        } else {
+            console.log(err);
+        }
+
+
+    });
+}
+
+function doWhatItSays() {
+    //grabbing information on random.txt file
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        //split it into two, option and parameter
+        var dataArray = data.split(",");
+        var option = (dataArray[0]);
+        var parameter = (dataArray[1]);
+        // console.log(option, parameter)
+
+        //if error, display
+        if (error) {
+            console.log(error);
+            //if movie-this, display and take as input for function
+        } else if (option == "movie-this") {
+            movieName = parameter;
+            movieThis()
+            //if spotify-this-song, display and take as input for function
+        } else if (option = "spotify-this-song") {
+            trackName = parameter;
+            spotifyThisSong()
+            //otherwise, display tweets
+        } else {
+            myTweets()
+        }
+    });
+}
+
+// console.log(command);
+// console.log(parameter)
+
+// parameter = parameter.replace('"', '');
+// parameter = parameter.replace('"', '');
+
+// console.log(parameter)
+// switch (command) {
+//     case 'my-tweets':
+//         Value = parameter;
+//         myTweets();
+//         break;
+//     case 'spotify-this-song':
+//         Value = parameter;
+//         spotify();
+//         break;
+
+//     case 'movie-this':
+//         Value = parameter;
+//         imdb();
+//         break;
+// }
+
+
